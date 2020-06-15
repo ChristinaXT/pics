54aee84a22a0496c098982a6e839f2c8b33e8e70
Christina Williams <christinaxtwilliams@gmail.com>
2020-06-15 14:27:55 -0400

    added axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
-  }
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID ySJsv8UlFFmeeWN_l37GO8rx8pEdAfUn1tXaUV-JLBU'
+      }
+  });
+}
+
+
+
 
   render() {
    return (
